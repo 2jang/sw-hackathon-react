f356e35fe6ae93496e623778cf7ba2e925775e42
2jang <jakeecc@naver.com>
2025-05-14 01:25:51 +0900

    임시 저장

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -18,182 +18,230 @@ import { featuresDataCollege, teamData, contactData } from "@/data";
 
 export function Home() {
   return (
-    <>
-      <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
-        <div className="absolute top-0 h-full w-full bg-[url('https://www.suwon.ac.kr/usr/images/suwon/college_top_technology.gif')] bg-cover bg-center" />
-        <div className="absolute top-0 h-full w-full bg-grey/60 bg-cover bg-center" />
-        <div className="max-w-8xl container relative mx-auto">
-          <div className="flex flex-wrap items-center">
-            <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
-              <Typography
-                variant="h1"
-                color="white"
-                className="mb-6 font-black"
-              >
-                지능형SW융합대학
-              </Typography>
-              <Typography variant="lead" color="white" className="opacity-80">
-                COLLEGE OF INTELLIGENT SOFTWARE CONVERGENCE <br/>
-                제4차 산업혁명, 수원대학교 지능형SW융합대학이 주도합니다.
-              </Typography>
-            </div>
-          </div>
-        </div>
-      </div>
-      <section className="-mt-52 bg-white px-4 pb-20 pt-4">
-        <div className="container mx-auto">
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            {featuresDataCollege.map(({ color, title, icon, description }) => (
-              <FeatureCardCollege
-                key={title}
-                color={color}
-                title={title}
-                icon={React.createElement(icon, {
-                  className: "w-5 h-5 text-white",
-                })}
-                description={description}
-              />
-            ))}
-          </div>
-          <div className="mt-32 flex flex-wrap items-center">
-            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
-              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-blue-gray-900 p-2 text-center shadow-lg">
-                <MapPinIcon className="h-8 w-8 text-white " />
-              </div>
-              <Typography
-                variant="h3"
-                className="mb-3 font-bold"
-                color="blue-gray"
-              >
-                캠퍼스 내 최적의 동선,<br/> 시간표를 효율적으로 계획하세요!
-              </Typography>
-              <Typography className="mb-8 font-normal text-blue-gray-500">
-                이동은 짧게, 수업은 알차게. 캠퍼스 길찾기로 시간표를 전략적으로 구성하세요!
-                <br />
-                <br />
-              </Typography>
-              <Button variant="filled">시작하기</Button>
-            </div>
-            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
-              <Card className="shadow-lg border shadow-gray-500/10 rounded-lg">
-                <CardHeader floated={false} className="relative h-56">
-                  <img
-                    alt="Card Image"
-                    src="/img/teamwork.png"
-                    className="h-full w-full"
-                  />
-                </CardHeader>
-                <CardBody>
-                  <Typography variant="small" color="blue-gray" className="font-normal">Special</Typography>
-                  <Typography
-                    variant="h5"
-                    color="blue-gray"
-                    className="mb-3 mt-2 font-bold"
-                  >
-                    수원대학교 길찾기 서비스
-                  </Typography>
-                  <Typography className="font-normal text-blue-gray-500">
-                     캠퍼스 내 건물 이동시간 알아보러가기
-                  </Typography>
-                </CardBody>
-              </Card>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="px-4 pt-20 pb-48">
-        <div className="container mx-auto">
-          <PageTitle section="교수 소개" heading="정보통신학과">
-            “정보통신의 길, 여러분의 가능성을 열어드립니다.”
-          </PageTitle>
-          <div className="mt-24 grid grid-cols-1 gap-12 gap-x-24 md:grid-cols-2 xl:grid-cols-4">
-            {teamData.map(({ img, name, position, socials }) => (
-              <TeamCard
-                key={name}
-                img={img}
-                name={name}
-                position={position}
-                socials={
-                  <div className="flex items-center gap-2">
-                    {socials.map(({ color, name }) => (
-                      <IconButton key={name} color={color} variant="text">
-                        <i className={`fa-solid text-xl fa-${name}`} />
-                      </IconButton>
-                    ))}
-                  </div>
-                }
-              />
-            ))}
-          </div>
-        </div>
-      </section>
-      <section className="relative bg-white py-24 px-4">
-        <div className="container mx-auto">
-          <PageTitle section="Co-Working" heading="Build something">
-            Put the potentially record low maximum sea ice extent tihs year down
-            to low ice. According to the National Oceanic and Atmospheric
-            Administration, Ted, Scambos.
-          </PageTitle>
-          <div className="mx-auto mt-20 mb-48 grid max-w-5xl grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
-            {contactData.map(({ title, icon, description }) => (
-              <Card
-                key={title}
-                color="transparent"
-                shadow={false}
-                className="text-center text-blue-gray-900"
-              >
-                <div className="mx-auto mb-6 grid h-14 w-14 place-items-center rounded-full bg-blue-gray-900 shadow-lg shadow-gray-500/20">
-                  {React.createElement(icon, {
-                    className: "w-5 h-5 text-white",
-                  })}
-                </div>
-                <Typography variant="h5" color="blue-gray" className="mb-2">
-                  {title}
-                </Typography>
-                <Typography className="font-normal text-blue-gray-500">
-                  {description}
-                </Typography>
-              </Card>
-            ))}
-          </div>
-          <PageTitle section="Contact Us" heading="Want to work with us?">
-            Complete this form and we will get back to you in 24 hours.
-          </PageTitle>
-          <form className="mx-auto w-full mt-12 lg:w-5/12">
-            <div className="mb-8 flex gap-8">
-              <Input variant="outlined" size="lg" label="Full Name" />
-              <Input variant="outlined" size="lg" label="Email Address" />
-            </div>
-            <Textarea variant="outlined" size="lg" label="Message" rows={8} />
-            <Checkbox
-              label={
+      <>
+        <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
+          <div className="absolute top-0 h-full w-full bg-[url('https://www.suwon.ac.kr/usr/images/suwon/college_top_technology.gif')] bg-cover bg-center" />
+          <div className="absolute top-0 h-full w-full bg-grey/60 bg-cover bg-center" />
+          <div className="max-w-8xl container relative mx-auto">
+            <div className="flex flex-wrap items-center">
+              <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
                 <Typography
-                  variant="small"
-                  color="gray"
-                  className="flex items-center font-normal"
+                    variant="h1"
+                    color="white"
+                    className="mb-6 font-black"
                 >
-                  I agree the
-                  <a
-                    href="#"
-                    className="font-medium transition-colors hover:text-gray-900"
-                  >
-                    &nbsp;Terms and Conditions
-                  </a>
+                  지능형SW융합대학
                 </Typography>
-              }
-              containerProps={{ className: "-ml-2.5" }}
-            />
-            <Button variant="gradient" size="lg" className="mt-8" fullWidth>
-              Send Message
-            </Button>
-          </form>
+                <Typography variant="lead" color="white" className="opacity-80">
+                  COLLEGE OF INTELLIGENT SOFTWARE CONVERGENCE <br/>
+                  제4차 산업혁명, 수원대학교 지능형SW융합대학이 주도합니다.
+                </Typography>
+              </div>
+            </div>
+          </div>
         </div>
-      </section>
-      <div className="bg-white">
-        <Footer />
-      </div>
-      <ChatbotUI />
-    </>
+        <section className="-mt-52 bg-white px-4 pb-20 pt-4">
+          <div className="container mx-auto">
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+              {featuresDataCollege.map(({ color, title, icon, description }) => (
+                  <div
+                      key={title}
+                      className="transition-all duration-300 hover:scale-105 hover:shadow-[0_0_20px_rgba(0,128,0,0.5)] rounded-lg overflow-hidden"
+                  >
+                    <FeatureCardCollege
+                        color={color}
+                        title={title}
+                        icon={React.createElement(icon, {
+                          className: "w-5 h-5 text-white",
+                        })}
+                        description={description}
+                    />
+                    <div className="flex mt-0 px-4 py-3 bg-white space-x-6">
+                      <a
+                          href="#"
+                          className="text-gray-900 font-medium hover:font-semibold transition-all text-left"
+                      >
+                        학과 소개 보기 >>
+                      </a>
+                      <a
+                          href="#"
+                          className="text-gray-900 font-medium hover:font-semibold transition-all text-left"
+                      >
+                        커리큘럼 보기 >>
+                      </a>
+                    </div>
+                  </div>
+              ))}
+            </div>
+            <div className="mt-32 flex flex-wrap items-center">
+              <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+                <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-blue-gray-900 p-2 text-center shadow-lg">
+                  <MapPinIcon className="h-8 w-8 text-white " />
+                </div>
+                <Typography
+                    variant="h3"
+                    className="mb-3 font-bold"
+                    color="blue-gray"
+                >
+                  캠퍼스 내 최적의 동선,<br/> 시간표를 효율적으로 계획하세요!
+                </Typography>
+                <Typography className="mb-8 font-normal text-blue-gray-500">
+                  이동은 짧게, 수업은 알차게. 캠퍼스 길찾기로 시간표를 전략적으로 구성하세요!
+                  <br />
+                  <br />
+                </Typography>
+                <Button variant="filled">시작하기</Button>
+              </div>
+              <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+                <div className="transition-all duration-300 hover:scale-105 hover:shadow-[0_0_20px_rgba(0,128,0,0.5)] rounded-lg overflow-hidden w-full">
+                  <Card className="rounded-lg border-0">
+                    <CardHeader floated={false} className="relative h-56">
+                      <img
+                          alt="Card Image"
+                          src="/img/teamwork.png"
+                          className="h-full w-full"
+                      />
+                    </CardHeader>
+                    <CardBody className="p-4">
+                      <Typography variant="small" color="blue-gray" className="font-normal">Special</Typography>
+                      <Typography
+                          variant="h5"
+                          color="blue-gray"
+                          className="mb-3 mt-2 font-bold"
+                      >
+                        수원대학교 길찾기 서비스
+                      </Typography>
+                      <Typography className="font-normal text-blue-gray-500">
+                        캠퍼스 내 건물 이동시간 알아보러가기
+                      </Typography>
+                      <div className="flex mt-4 space-x-6">
+                        <a
+                            href="#"
+                            className="text-gray-900 font-medium hover:font-semibold transition-all text-left"
+                        >
+                          서비스 이용하기 >>
+                        </a>
+                        <a
+                            href="#"
+                            className="text-gray-900 font-medium hover:font-semibold transition-all text-left"
+                        >
+                          자세히 알아보기 >>
+                        </a>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="px-4 pt-20 pb-48 bg-[#f7f8fa]">
+          <div className="container mx-auto">
+            <PageTitle section="교수 소개" heading="지능형SW융합대학">
+              "정보통신의 길, 여러분의 가능성을 열어드립니다."
+            </PageTitle>
+            <div className="mt-24 grid grid-cols-1 gap-12 gap-x-24 md:grid-cols-2 xl:grid-cols-4">
+              {teamData.map(({ img, name, position, socials }) => (
+                  <div
+                      key={name}
+                      className="transition-all duration-300 hover:scale-105 hover:shadow-[0_0_20px_rgba(0,128,0,0.5)] rounded-lg overflow-hidden"
+                  >
+                    <Card className="rounded-lg border-0">
+                      <div className="h-72 w-full overflow-hidden">
+                        <img
+                            src={img}
+                            alt={name}
+                            className="h-full w-full object-cover"
+                        />
+                      </div>
+                      <CardBody className="p-4 text-center">
+                        <Typography variant="h5" color="blue-gray" className="mb-1 font-bold">
+                          {name}
+                        </Typography>
+                        <Typography className="font-normal text-blue-gray-500">
+                          {position}
+                        </Typography>
+                        <div className="flex justify-center gap-2 mt-3">
+                          {socials.map(({ color, name }) => (
+                              <IconButton key={name} color={color} variant="text">
+                                <i className={`fa-solid text-xl fa-${name}`} />
+                              </IconButton>
+                          ))}
+                        </div>
+                      </CardBody>
+                    </Card>
+                  </div>
+              ))}
+            </div>
+          </div>
+        </section>
+        <section className="relative bg-white py-24 px-4">
+          <div className="container mx-auto">
+            <PageTitle section="Co-Working" heading="Build something">
+              Put the potentially record low maximum sea ice extent tihs year down
+              to low ice. According to the National Oceanic and Atmospheric
+              Administration, Ted, Scambos.
+            </PageTitle>
+            <div className="mx-auto mt-20 mb-48 grid max-w-5xl grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
+              {contactData.map(({ title, icon, description }) => (
+                  <Card
+                      key={title}
+                      color="transparent"
+                      shadow={false}
+                      className="text-center text-blue-gray-900"
+                  >
+                    <div className="mx-auto mb-6 grid h-14 w-14 place-items-center rounded-full bg-blue-gray-900 shadow-lg shadow-gray-500/20">
+                      {React.createElement(icon, {
+                        className: "w-5 h-5 text-white",
+                      })}
+                    </div>
+                    <Typography variant="h5" color="blue-gray" className="mb-2">
+                      {title}
+                    </Typography>
+                    <Typography className="font-normal text-blue-gray-500">
+                      {description}
+                    </Typography>
+                  </Card>
+              ))}
+            </div>
+            <PageTitle section="Contact Us" heading="Want to work with us?">
+              Complete this form and we will get back to you in 24 hours.
+            </PageTitle>
+            <form className="mx-auto w-full mt-12 lg:w-5/12">
+              <div className="mb-8 flex gap-8">
+                <Input variant="outlined" size="lg" label="Full Name" />
+                <Input variant="outlined" size="lg" label="Email Address" />
+              </div>
+              <Textarea variant="outlined" size="lg" label="Message" rows={8} />
+              <Checkbox
+                  label={
+                    <Typography
+                        variant="small"
+                        color="gray"
+                        className="flex items-center font-normal"
+                    >
+                      I agree the
+                      <a
+                          href="#"
+                          className="font-medium transition-colors hover:text-gray-900"
+                      >
+                        &nbsp;Terms and Conditions
+                      </a>
+                    </Typography>
+                  }
+                  containerProps={{ className: "-ml-2.5" }}
+              />
+              <Button variant="gradient" size="lg" className="mt-8" fullWidth>
+                Send Message
+              </Button>
+            </form>
+          </div>
+        </section>
+        <div className="bg-white">
+          <Footer />
+        </div>
+        <ChatbotUI />
+      </>
   );
 }
 
