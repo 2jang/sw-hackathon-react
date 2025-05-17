9cad85f41213573f2a8cfc8d9dfb51dc7cb33990
2jang <jakeecc@naver.com>
2025-05-17 16:59:21 +0900

    vite config 수정

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
   server: {
-    allowedHosts: ['notably-sure-hippo.ngrok-free.app', 'dev.2jang.me'],
+    allowedHosts: ['notably-sure-hippo.ngrok-free.app', 'dev.2jang.me','suwonai.store'],
     proxy: {
       // '/api'로 시작하는 모든 요청을 target으로 전달
       '/api': {
